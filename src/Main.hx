e484139571102109c1e9667982004a978d74f365
Joe Spataro <jspataro@gmail.com>
2018-09-25 13:32:30 -0700

    Adding some stars.

--- src/Main.hx
+++ src/Main.hx
@@ -7,7 +7,8 @@ using extensions.ObjectExtensions;
 using extensions.LibraryExtensions;
 
 class Main extends hxd.App {
-	public static var world(default, null) : B2World;
+	public static var physics_world(default, null) : B2World;
+	public var world : World;
 
 	static var physics_time_step : Float = 1.0 / 60.0;
 	static var velocity_iterations : Int = 6;
@@ -15,16 +16,16 @@ class Main extends hxd.App {
 
 	override function new(physicsWorld:B2World) {
 		wantedFPS = 60.0;		
-		world = physicsWorld;
+		physics_world = physicsWorld;
 
 		super();
 	}
 
 	override function init() {
-		
+		var world = new h3d.scene.World(64, 128, s3d);
 		var model = hxd.Res.models.test.icosahedron.toHmd();
 		var obj = model.makeObject();
-		s3d.addChild(obj);
+		world.addChild(obj);
 
 		if (model.HasVertexColors()) {
 			obj.UseVertexColor();
@@ -32,6 +33,17 @@ class Main extends hxd.App {
 
 		s3d.camera.pos.set( -20, -5, 20);
 		s3d.camera.target.z += 1;
+
+		var particles = new h3d.parts.GpuParticles(world);
+		var group = particles.addGroup();
+		group.size = 0.2;
+		group.gravity = 0;
+		group.speed = 0.01;
+		group.life = 100;
+		group.emitAngle = 0;
+		group.nparts = 1000;
+		group.emitMode = CameraBounds;
+		particles.volumeBounds = h3d.col.Bounds.fromValues( -20, -20, 15, 40, 40, 40);
 
 		// add lights and setup materials
 		var dir = new DirLight(new h3d.Vector( -1, 3, -10), s3d);
@@ -45,7 +57,7 @@ class Main extends hxd.App {
 	}
 
 	override function update(dt:Float) {
-		world.step(
+		physics_world.step(
 			physics_time_step, 
 			velocity_iterations, 
 			position_iterations);
